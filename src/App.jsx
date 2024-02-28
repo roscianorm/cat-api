79a9ac3cf5276b4584656476587fd8dbef5f8b0d
Miguel Rosciano <roscianorm@gmail.com>
2024-02-28 11:17:52 -0600

    first part of the test done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from 'react';
+
 import '/style.css';
 
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat';
+
 export default function App() {
-	return <h1>Hello!!!</h1>;
+	const [fact, setFact] = useState();
+	useEffect(() => {
+		fetch(CAT_ENDPOINT_RANDOM_FACT)
+			.then((res) => res.json())
+			.then((data) => {
+				const fact = data.fact;
+				setFact(fact);
+
+				const firstWord = fact.split(' ')[0];
+				const firstThreeWords = fact.split(' ').slice(0, 3).join(' ');
+				console.log({ firstWord, firstThreeWords });
+			})
+			.catch((err) => {
+				setFact('An error occurred, please try again.');
+				console.error(err);
+			});
+	}, []);
+
+	const [image, setImage] = useState();
+	useEffect(() => {
+		fetch(CAT_ENDPOINT_IMAGE_URL).then((res) => setImage(res.url));
+	}, []);
+
+	return (
+		<main>
+			<h1>Cats App 🐈‍⬛</h1>
+			<img src={image} alt="" width="450px" />
+			{fact && <p>{fact}</p>}
+		</main>
+	);
 }
